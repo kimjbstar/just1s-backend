37c015f094f3825e47e2eb77d1e7d087290b9376
kimjbstar <kimjbstar@gmail.com>
2020-04-25 17:44:55 +0900

    테스트 프레임워크 mocha로 변경

--- tests/review.service.test.ts
+++ tests/review.service.test.ts
@@ -8,6 +8,7 @@ import { UtilService } from "@src/services/util.service";
 import { Review } from "@src/models/review.model";
 import { Store } from "@src/models/store.model";
 import { Test } from "@nestjs/testing";
+import { expect, should } from "chai";
 
 export type SequelizeModelCtor = ModelCtor<Model<any, any>>;
 
@@ -18,66 +19,64 @@ export interface SequelizeModelCtorMap {
 describe("ReviewsService", () => {
   let sequelize: Sequelize;
   let reviewsService: ReviewsService;
-  let utilService: UtilService;
 
-  beforeAll(() => {
-    // const moduleRef = await Test.createTestingModule({
-    //   controllers: [],
-    //   providers: [ReviewsService, UtilService]
-    // }).compile();
-    // reviewsService = moduleRef.get<ReviewsService>(ReviewsService);
-    // process.env.NODE_ENV = "test";
-    // sequelize = new Sequelize({
-    //   storage: path.join(
-    //     process.cwd(),
-    //     "tests",
-    //     process.env.SEQUELIZE_DATABASE + "_test.db"
-    //   ),
-    //   host: "localhost",
-    //   dialect: "sqlite",
-    //   models: [path.join(process.cwd(), "src", "models")],
-    //   modelMatch: (_filename, _member) => {
-    //     const filename = inflection.camelize(_filename.replace(".model", ""));
-    //     const member = _member;
-    //     return filename === member;
-    //   },
-    //   logging: false
-    // });
-    // await sequelize.authenticate();
-    // expect(sequelize).toBeDefined();
-    // await child_process.execSync(`npx sequelize db:migrate --env test`);
-    // await sequelize.truncate();
-    // await sequelize.query(`UPDATE SQLITE_SEQUENCE SET seq = 0`);
-    // const fixtureDir = path.join(process.cwd(), "/tests/fixtures");
-    // const models: SequelizeModelCtor[] = [Store, Review];
-    // for (let modelCtor of models) {
-    //   const fixtureJSON = await fs.readFileSync(
-    //     path.join(fixtureDir, modelCtor.tableName)
-    //   );
-    //   const rows = JSON.parse(fixtureJSON.toString());
-    //   const insertedRows = await modelCtor.bulkCreate(rows);
-    //   // console.log(insertedRows);
-    // }
+  before(async () => {
+    // set test db
+    const moduleRef = await Test.createTestingModule({
+      controllers: [],
+      providers: [ReviewsService, UtilService]
+    }).compile();
+    reviewsService = moduleRef.get<ReviewsService>(ReviewsService);
+    process.env.NODE_ENV = "test";
+    sequelize = new Sequelize({
+      storage: path.join(
+        process.cwd(),
+        "tests",
+        process.env.SEQUELIZE_DATABASE + "_test.db"
+      ),
+      host: "localhost",
+      dialect: "sqlite",
+      models: [path.join(process.cwd(), "src", "models")],
+      modelMatch: (_filename, _member) => {
+        const filename = inflection.camelize(_filename.replace(".model", ""));
+        const member = _member;
+        return filename === member;
+      },
+      logging: false
+    });
+    await sequelize.authenticate();
+    expect(sequelize).exist;
+
+    // truncate
+    await child_process.execSync(`npx sequelize db:migrate --env test`);
+    await sequelize.truncate();
+    await sequelize.query(`UPDATE SQLITE_SEQUENCE SET seq = 0`);
+
+    // bulkCreate
+    const fixtureDir = path.join(process.cwd(), "/tests/fixtures");
+    const models: SequelizeModelCtor[] = [Store, Review];
+    for (let modelCtor of models) {
+      const fixtureJSON = await fs.readFileSync(
+        path.join(fixtureDir, modelCtor.tableName)
+      );
+      const rows = JSON.parse(fixtureJSON.toString());
+      await modelCtor.bulkCreate(rows);
+    }
   });
 
-  test("11", () => {
-    expect(1).toBe(1);
+  it("find", async () => {
+    const reviews = await reviewsService.find({});
+    expect(reviews.length).greaterThan(0);
   });
 
-  // test("find", async () => {
-  //   const reviews = await reviewsService.find({});
-  //   console.log(reviews);
-  //   expect(1).toBe(1);
-  // });
+  it("create", async () => {
+    const review = await reviewsService.create({
+      title: "review_title"
+    });
+    expect(review["title"]).equal("review_title");
+  });
 
-  // test("create", async () => {
-  //   const review = await reviewsService.create({
-  //     title: "review_title"
-  //   });
-  //   expect(1).toBe(1);
-  // });
-
-  afterAll((done) => {
+  after((done) => {
     // sequelize.close();
     done();
   });
