00e5b3981e77e2fb3ffff503b92d757ece076cdd
kimjbstar <kimjbstar@gmail.com>
2020-05-12 13:27:57 +0900

    user create dto 수정 및 단위 테스트 추가

--- src/modules/users/dtos/user-create.dto.ts
+++ src/modules/users/dtos/user-create.dto.ts
@@ -3,7 +3,6 @@ import { UserSNSType, UserStatus } from "../users.enum";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class UserCreateDto {
-  @IsNotEmpty()
   @IsEnum(UserSNSType)
   @ApiProperty({
     description: "UserSNSType(를) 입력해주세요 !",
@@ -11,7 +10,6 @@ export class UserCreateDto {
   })
   snsType: UserSNSType;
 
-  @IsNotEmpty()
   @IsEnum(UserStatus)
   @ApiProperty({
     description: "status을(를) 입력해주세요 !",
@@ -26,56 +24,8 @@ export class UserCreateDto {
   })
   email: string;
 
-  @IsNotEmpty()
-  @ApiProperty({
-    description: "stringId을(를) 입력해주세요 !"
-  })
-  stringId: string;
-
-  @IsNotEmpty()
-  @ApiProperty({
-    description: "pw을(를) 입력해주세요 !"
-  })
-  pw: string;
-
-  @IsNotEmpty()
-  @ApiProperty({
-    description: "imgUrl을(를) 입력해주세요 !"
-  })
-  imgUrl: string;
-
-  @IsNotEmpty()
-  @ApiProperty({
-    description: "nickname을(를) 입력해주세요 !"
-  })
-  nickname: string;
-
-  @IsNotEmpty()
   @ApiProperty({
     description: "name을(를) 입력해주세요 !"
   })
   name: string;
-
-  @IsNotEmpty()
-  @ApiProperty({
-    description: "phoneNumber을(를) 입력해주세요 !"
-  })
-  phoneNumber: string;
-
-  @IsNotEmpty()
-  @ApiProperty({
-    description: "desc을(를) 입력해주세요 !"
-  })
-  desc: string;
-
-  @IsNotEmpty()
-  @ApiProperty({
-    description: "blockedUntil을(를) 입력해주세요 !"
-  })
-  blockedUntil: Date;
-  @IsNotEmpty()
-  @ApiProperty({
-    description: "toGetPushed을(를) 입력해주세요 !"
-  })
-  toGetPushed: boolean;
 }
---
+++ tests/user.service.test.ts
@@ -0,0 +1,42 @@
+import { Test } from "@nestjs/testing";
+import { expect } from "chai";
+import { DecksService } from "@src/modules/decks/decks.service";
+import { UsersService } from "@src/modules/users/users.service";
+import { Connection } from "typeorm";
+import { UtilService } from "@src/services/util.service";
+import { importAndGetConn } from "./common";
+import { MusicsService } from "@src/modules/music/music.service";
+
+describe("user.service.ts", () => {
+  let usersService: UsersService;
+  let conn: Connection;
+
+  before(async () => {
+    conn = await importAndGetConn();
+    const moduleRef = await Test.createTestingModule({
+      controllers: [],
+      providers: [
+        DecksService,
+        UsersService,
+        UsersService,
+        UtilService,
+        MusicsService
+      ]
+    }).compile();
+    usersService = moduleRef.get<UsersService>(UsersService);
+  });
+
+  beforeEach(async () => {});
+
+  it("create - User 추가", async () => {
+    const user = await usersService.create({
+      snsType: "FACEBOOK",
+      name: "아으"
+    });
+    console.log(user);
+  });
+
+  after(async () => {
+    await conn.close();
+  });
+});
