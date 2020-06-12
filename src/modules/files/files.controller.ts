89b0493f18d21bad715eb270d8262ede0c210714
kimjbstar <kimjbstar@gmail.com>
2020-06-12 12:11:05 +0900

    re

--- src/modules/files/files.controller.ts
+++ src/modules/files/files.controller.ts
@@ -52,7 +52,7 @@ export class FilesController {
 
     // 열화 처리
     const s3 = new S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       region: "ap-northeast-2"
     });
