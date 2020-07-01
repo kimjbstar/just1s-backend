2af0732f31b9442219d5aed323c96ff7c18ba3e9
kimjbstar <kimjbstar@gmail.com>
2020-07-01 18:06:16 +0900

    webpack 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,75 @@ import { DecksService } from "./modules/decks/decks.service";
 import { MusicsService } from "./modules/music/music.service";
 import { PassportModule } from "@nestjs/passport";
 import { Connection } from "typeorm";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+const entityContexts = (require as any).context(
+  "./entities",
+  true,
+  /\.entity.ts$/
+);
+const entities = entityContexts
+  .keys()
+  .map((modulePath) => entityContexts(modulePath))
+  .reduce(
+    (result, entityModule) =>
+      result.concat(Object.keys(entityModule).map((key) => entityModule[key])),
+    []
+  );
+console.log(entities);
+
+const migrationContexts = (require as any).context(
+  "./migration",
+  true,
+  /\.ts$/
+);
+const migrations = migrationContexts
+  .keys()
+  .map((modulePath) => migrationContexts(modulePath))
+  .reduce(
+    (result, migrationModule) =>
+      result.concat(
+        Object.keys(migrationModule).map((key) => migrationModule[key])
+      ),
+    []
+  );
+console.log(migrations);
+
+const subscriberContexts = (require as any).context(
+  "./subscribers",
+  true,
+  /\.subscriber.ts$/
+);
+const subscribers = subscriberContexts
+  .keys()
+  .map((modulePath) => subscriberContexts(modulePath))
+  .reduce(
+    (result, subscriberModule) =>
+      result.concat(
+        Object.keys(subscriberModule).map((key) => subscriberModule[key])
+      ),
+    []
+  );
+console.log(subscribers);
+
+const typeORMConfig: TypeOrmModuleOptions = {
+  type: "mysql",
+  host: process.env.TYPEORM_HOST,
+  port: Number(process.env.TYPEORM_PORT),
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+  entities: entities,
+  migrations: migrations,
+  subscribers: subscribers,
+  cli: {
+    entitiesDir: "src/entities",
+    migrationsDir: "src/migration"
+  },
+  synchronize: false,
+  logging: true
+};
+console.log(typeORMConfig);
 
 @Module({
   imports: [
@@ -15,7 +83,7 @@ import { TypeOrmModule } from "@nestjs/typeorm";
       signOptions: { expiresIn: "30m" }
     }),
     PassportModule,
-    TypeOrmModule.forRoot()
+    TypeOrmModule.forRoot(typeORMConfig)
   ],
   controllers: [AppController],
   providers: [UsersService, DecksService, MusicsService],
@@ -23,6 +91,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 })
 export class AppModule {
   constructor(private connection: Connection) {
+    this.connection.showMigrations().then((a) => {
+      console.log("pending migration exists??", a);
+    });
     this.connection.runMigrations().then((migrations) => {
       console.debug(migrations);
     });
